cc74fc2d2c216edfe8d7d08458a1bb2d804c03e8
kzri4 <kazu4126ab@gmail.com>
2020-10-24 22:09:57 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+define('DSN','mysql:host=db;dbname=pet_shop;charset=utf8;');
+define('USER','staff');
+define('PASSWORD','9999');
+
+try {
+    $dbh = new PDO(DSN, USER, PASSWORD);
+
+} catch (PDOException $e) {
+    // 接続がうまくいかない場合こちらの処理がなされる
+    echo $e->getMessage();
+    exit;
+}
+
+if($_GET['keyword'] ===''){
+    $sql = 'SELECT * FROM animals';
+
+    $stmt = $dbh->prepare($sql);
+
+}else{
+    $keyword = $_GET["keyword"];
+    
+    $keyword = '%' . $_GET["keyword"] . '%';
+
+    $sql = 'SELECT * FROM animals WHERE description LIKE :keyword';
+
+    $stmt = $dbh->prepare($sql);
+    
+    $stmt->bindParam(':keyword', $keyword, PDO::PARAM_STR);
+}
+
+    $stmt->execute();
+
+    $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang = "ja">
+
+<head>
+    <meta charaset = "UTF-8">
+    <title>ペットショップ</title>
+</head>
+
+<body>
+    <h1>本日のご紹介ペット！</h1>
+
+            <form action="index.php" method="get">
+
+            キーワード: <input type="text" name="comment" placeholder="キーワードの入力">
+                    <input type="submit" name="submit" value="検索">
+                    <br><br>
+
+                    <?php foreach ($animals as $animal) :?>
+                        <?= $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん'  ?><br>
+                        <?= $animal['description'] ?><br> 
+                        <?= $animal['birthday'] . '生まれ' ?><br> 
+                        <?= '出身地' . $animal['birthplace']  ?><hr>
+                    <?php endforeach; ?>    
+            </form>
+</body>
+
+</html>
